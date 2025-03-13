8d1f20a16afaf3035a4d034a4b094ed0f68b72e7
Павел Яковлев <pablo@pablo.pablo>
2025-03-13 12:21:50 +0300

    Артему на доработку

--- main/views.py
+++ main/views.py
@@ -1,6 +1,19 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ApartmentForm
+from .models import Apartment
 
+def add_apartment(request):
+    if request.method == 'POST':
+        form = ApartmentForm(request.POST)
+        if form.is_valid():
+            form.request = request
+            form.save()
+            return redirect('apartments_list')
+    else:
+        form = ApartmentForm()
+    return render(request, 'pages/Flat_add.html', {'form': form})
 
 def index_page(request: WSGIRequest):
     context = {
