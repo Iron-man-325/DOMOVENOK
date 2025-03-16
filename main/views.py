69935012a3da26aae97de374cefc00584055aad2
Алабин Юрий <you@example.com>
2025-03-16 22:28:50 +0300

    view_update

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .models import Profile
+from .forms import UserForm, User, SettingsForm
 
 
 def index_page(request: WSGIRequest):
@@ -13,5 +16,44 @@ def login_page(request: WSGIRequest):
     raise NotImplementedError
 
 
-def registration_page(request: WSGIRequest):
-    raise NotImplementedError
+def registration_page(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                email=form.data["email"],
+                username=form.data["username"],
+                password=form.data["password"],
+                last_name=form.data["last_name"]
+            )
+
+            profile = Profile()  # Создание объекта профиля (в ОЗУ)
+            profile.user = user
+            profile.save()  # Фиксирует профиль в БД
+
+            login(request, user)
+            return redirect('profile')
+    form = UserForm()
+
+    data = {
+        'form': form,
+    }
+    return render(request, "HTML/creating-ak.html", data)
+
+def login_view(request):
+    context = {
+        "error": None
+    }
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        context["error"] = "Неверное имя пользователя или пароль."
+
+    return render(request, "registration/login.html", context)
