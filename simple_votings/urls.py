a8e23d2a0fe29419607e3ba0d74dbdf01e473ec8
Черкасов Сергей aka seryi_otten0k <scerkasov0101@gmail.com>
2025-04-20 11:28:52 +0300

    Рефакторинг urls.py

--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -8,11 +8,11 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('', views.index_page, name='index'),
-     path('admin/', views.apartment_list, name='apartment_list'),
-     path('',views.add_apartment, name='main'),
-     path('login/', views.login_view, name='login'),
-     path('flat/<int:flat_id>/', views.show_flat, name='flat_detail'),
-     # path('registration/', views.registration_page, name='registration'),
+    path('admin/', views.apartment_list, name='apartment_list'),
+    path('', views.add_apartment, name='main'),
+    path('login/', views.login_view, name='login'),
+    path('flat/<int:flat_id>/', views.show_flat, name='flat_detail'),
+    # path('registration/', views.registration_page, name='registration'),
     # path('login/', views.login_page, name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
